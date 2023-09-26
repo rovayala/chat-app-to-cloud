39a9ee69cb02b15203d21cc200a789189493c9b3
rovrovayala <rinarov11@gmail.com>
2023-09-26 14:45:40 +0300

    deploy step3

--- deploy1.sh
+++ deploy1.sh
@@ -1,50 +1,57 @@
 #!/bin/bash
 
-
-# Prompt user for version and commit hash
-read -p "Enter the version: " version
-read -p "Enter the commit hash: " commit_hash
-
-
-# Check if the image exists.
-IMAGE_EXISTS=$(docker images --format="table {{.Repository}}:{{.Tag}}" | grep "chat-app:$version")
-
-# Ask the user if they want to rebuild the image or use the existing one.
-if [[ -n "$IMAGE_EXISTS" ]]; then
-    echo "The image 'chat-app:$version' already exists."
-    read -p "Do you want to rebuild the image? (y/n): " REBUILD_IMAGE
-
-    if [[ "$REBUILD_IMAGE" == "y" ]]; then
-        # Delete the existing image.
-        docker rmi chat-app:"$version"
-    fi
+# Check if the script was passed any arguments
+if [ $# -eq 0 ]; then
+    # No arguments were passed, so prompt the user for them
+    # Get the version and commit hash from the user
+    echo "Enter the version number: "
+    read version
+    echo "Enter the commit hash: "
+    read commit_hash
+else
+  # Arguments were passed, so use them
+  version=$1
+  commit_hash=$2
 fi
 
-# Build the image with a tag
-docker build -t chat-app:"$version" . || { echo "Failed to build the image"; exit 1; }
+if [ $(git rev-list --quiet $commit_hash) -eq 0 ]; then
+    echo "No such commit hash"
+    exit 1
+fi
 
-# Tag the image for your registry 
-docker tag chat-app:"$version" rivkarizel/chat-app:"$version" || { echo "Failed to tag for registry"; exit 1; }
-
-# Push the image to the registry 
-docker push rivkarizel/chat-app:"$version" || { echo "Failed to push to registry"; exit 1; }
-
-# Ask the user if they want to tag and push the image to the GitHub repository.
-read -p "Do you want to tag and push the image to the GitHub repository? (y/n): " TAG_GITHUB_REPO
-
-if [[ "$TAG_GITHUB_REPO" == "y" ]]; then
-    # Check if the commit hash exists.
-    COMMIT_HASH=$(git rev-parse HEAD)
-
-    if [[ -n "$COMMIT_HASH" ]]; then
-        # Tag the image with the commit hash.
-        git tag "$version" "$commit_hash"
-
-        # Push the tag to the GitHub repository.
-        git push origin "$version"
+version="v$version"
+dock_name='Dockerfile'
+if [ $# -eq 3 ]; then
+    dock_name=$3
+fi
+appname="chat-app"
+if docker image inspect "$appname:$version" &> /dev/null; then
+    read -p "Image $appname:$version already exists. Do you want to rebuild it? (y/n): " rebuild
+    if [[ "$rebuild" == "y" ]]; then
+    # Delete the existing image
+        docker rmi "$appname:$version"
+        # Build the Docker image
+        docker build -t ${appname}:${version} . -f ${dock_name}
     else
-        echo "The commit hash does not exist."
+        echo "Using existing image $appname:$version."
     fi
+else 
+    docker build -t ${appname}:${version} . -f ${dock_name}
 fi
 
-echo "Deployment successful!"
+read -p "Do you want to push the tag to GitHub? (y/n): " push_tag
+if [[ "$push_tag" == "y" ]]; then
+    # Tag the image with the commit hash
+    git tag ${version} ${commit_hash}
+    git push origin ${version}
+    # Check if the image was pushed successfully
+    if [ $? -ne 0 ]; then
+        echo "Error pushing image to GitHub"
+        exit 1
+    else
+        # Success!
+        echo "Image pushed to GitHub successfully"
+    fi
+else
+    echo "Tag not pushed to GitHub."
+fi
